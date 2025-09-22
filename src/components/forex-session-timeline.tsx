67528406683e70b530ca657efa0d57fc96c928a4
Jake Garbo <jakegarbo21@gmail.com>
2025-09-22 00:38:17 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/components/forex-session-timeline.tsx
+++ src/components/forex-session-timeline.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { cn } from '@/lib/utils';
 import { AlertTriangle, MapPin } from 'lucide-react';
 import {
